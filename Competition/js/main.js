b2aa4188d87d5173e0cad4bd65a5e3a318836ea2
Dr <rcolasanti@bournemouth.ac.uk>
2021-02-27 19:42:56 +0000

    CACanvas

--- Competition/js/main.js
+++ Competition/js/main.js
@@ -1,56 +1,23 @@
-function keypress(e,element,output){
-    if(e.keyCode === 13){
-        e.preventDefault(); // Ensure it is only this code that runs
-        game.parse(element.value);
-        element.value = ""
-        let out = document.getElementById(output);
-        out.scrollTop = out.scrollHeight;
+class CACanvas{
+    constructor(size,cSize){
+        this.size = size;
+        this.cSize = cSize;
+        this.buffer = document.createElement("canvas");
+        this.buffer.width = this.size*this.cSize;
+        this.buffer.height = this.size*this.cSize;
+        this.ctx = this.buffer.getContext("2d");
+    }
+
+    draw(x,y,cellColor){
+        this.ctx.beginPath();
+        this.ctx.rect(x*this.cSize,y*this.cSize,this.cSize,this.cSize)
+        this.ctx.fillStyle = cellColor;
+        this.ctx.fill();  
+    }
+
+    update(canvasID){
+        var visible_canvas = document.getElementById(canvasID);
+        var vctx = visible_canvas.getContext("2d");
+        vctx.drawImage(this.buffer, 0, 0, this.ctx.canvas.width, this.ctx.canvas.height, 0, 0, vctx.canvas.width, vctx.canvas.height); 
     }
 }
-document.getElementById('output').value = '';
-document.getElementById('command').focus();;
-
-
-class Parser{
-    constructor(nouns,verbs){
-        this.nouns = nouns;
-        this.verbs = verbs;
-    }
-    parse(input){
-        input = input.replace(/[\n\r]+/g, '').trim();// remove cr
-        let words = input.split(/\ +/) // regex with more than one space
-        let lWords = words.map(w=>{return w.toLowerCase()})
-        if(words.length!=2){
-            return{msg:"must have two words",verb:"",noun:""};
-        }
-        let verb =  this.verbs.indexOf(lWords[0])
-        let noun =  this.nouns.indexOf(lWords[1])
-        if(verb ==-1){
-            return{msg:lWords[0]+" not in verbs",verb:lWords[0],noun:lWords[1]};    
-        }
-        if(noun == -1){
-            return{msg:lWords[1]+" not in nouns",verb:lWords[0],noun:lWords[1]};    
-        }
-        return{msg:"ok",verb:this.verbs[verb],noun:this.nouns[noun]};
-    }
-}
-
-
-class Game{
-    constructor(){
-        let nouns = ["box","folder","north","south","east","west"]
-        let verbs = ["go","move","take","drop"]
-        this.parser = new Parser(nouns,verbs)
-    }
-    parse(words){
-        let result = this.parser.parse(words);
-        let out = document.getElementById('output');
-        if(result.msg=="ok"){    
-            out.value +=  result.verb+ " "+ result.noun+"\n";
-        }else{
-            out.value += result.msg + "\n";
-        }
-    }
-}
-
-var game = new Game();
