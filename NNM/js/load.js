80d83529d1437134fb66d00c47547904805f7b9a
ric <rcolasanti@bournemouth.ac.uk>
2021-07-12 13:48:54 +0100

    NNM

---
+++ NNM/js/load.js
@@ -0,0 +1,11 @@
+function loadDoc(id,page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(id).innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET",page, true);
+    xhttp.send();
+}
